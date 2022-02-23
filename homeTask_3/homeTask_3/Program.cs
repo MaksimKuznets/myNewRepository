def93328cbe928a5bca17d175ef8c32647d1b24f
Maksim Kuznets <kashimby@gmail.com>
2022-02-23 23:37:39 +0300

    homeTask3 v 1.1

--- homeTask_3/homeTask_3/Program.cs
+++ homeTask_3/homeTask_3/Program.cs
@@ -4,30 +4,42 @@ namespace homeTask_3
 {
     class Program
     {
-        
+
         //Задание- создать три массива по 10 элементов, первый
         //заполнить значениями индексов элементов, второй - квадратами индексов,
         //третий-кубами, и вывести на экран средние значения элементов для всех массивов
 
-        static double averageArray(int[] array, int n)
+        static void InitializeArray(int[] array, int n)
+        {
+            for (int i = 0; i < array.Length; i++)
+            
+                array[i] = (int)Math.Pow(i+1, n);
+                        
+        }
+
+        static double AverageSumArray(int[] array)
         {
             double sum = 0;
-            for ( int i=0; i<array.Length; i++)
-            {
-                array[i] = (int)Math.Pow(i, n);
+            for (int i = 0; i < array.Length; i++)
+            
                 sum += array[i];
-            }
-         
+            
             return sum / array.Length;
         }
+
+
+
         static void Main(string[] args)
         {
             int[] array1 = new int[10];
             int[] array2 = new int[10];
             int[] array3 = new int[10];
-            Console.WriteLine("Average sum for {0} massive is {1}", 1, averageArray(array1, 1));
-            Console.WriteLine("Average sum for {0} massive is {1}", 2, averageArray(array2, 2));
-            Console.WriteLine("Average sum for {0} massive is {1}", 3, averageArray(array3, 3));
+            InitializeArray(array1, 1);
+            InitializeArray(array2, 2);
+            InitializeArray(array3, 3);
+            Console.WriteLine("Average sum for array1 ={0} ", AverageSumArray(array1));
+            Console.WriteLine("Average sum for array2 ={0} ", AverageSumArray(array2));
+            Console.WriteLine("Average sum for array3 ={0} ", AverageSumArray(array3));
 
 
 
