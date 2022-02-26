e7ece7bff0bda29d243d0534bc71462411526e95
MaksimKuznets <99362623+MaksimKuznets@users.noreply.github.com>
2022-02-27 00:30:59 +0300

    HomeTask_2 (#3)
    * hometask2
    
    * homeTask2 v 1.1

---
+++ homeTask_2/homeTask_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace homeTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать массив размером 10 элементов, заполнить его квадратами
+            //индексов(в цикле), и вывести его содержимое на экран
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = (int)Math.Pow(i,2);
+                Console.Write("Element of massive {0}\n", array[i]);
+            }
+           
+        }
+    }
+}
