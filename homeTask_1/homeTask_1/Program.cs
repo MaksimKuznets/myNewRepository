46c843fdefd71b23afe3986d2063e928564e6f24
Maksim Kuznets <kashimby@gmail.com>
2022-02-27 00:34:11 +0300

    homeTask1 v 1.1

--- homeTask_1/homeTask_1/Program.cs
+++ homeTask_1/homeTask_1/Program.cs
@@ -9,25 +9,9 @@ namespace homeTask_1
             //Console.WriteLine("Hello World!");
             //Задача- вывести в консоль максимальные и минимальные допустимые значения для
             //типов данных byte, short, int, long, float, double, uint, ulong.
-            byte bMax = byte.MaxValue;
-            byte bMin = byte.MinValue;
-            short sMax = short.MaxValue;
-            short sMin = short.MinValue;
-            int iMax = int.MaxValue;
-            int iMin = int.MinValue;
-            long lMax = long.MaxValue;
-            long lMin = long.MinValue;
-            float fMax = float.MaxValue;
-            float fMin = float.MinValue;
-            double dMax = double.MaxValue;
-            double dMin = double.MinValue;
-            uint uiMax = uint.MaxValue;
-            uint uiMin = uint.MinValue;
-            ulong ulMax = ulong.MaxValue;
-            ulong ulMin = ulong.MinValue;
-            Console.WriteLine("Max Value of Byte Type  = {0}\n" +
-              "Min Value of byte type  = {1}\n" +
-              "Max value of short type = {2}\n" +
+              Console.WriteLine("Max Value of Byte Type  = {0}\n" +
+                "Min Value of byte type  = {1}\n" +
+                "Max value of short type = {2}\n" +
                 "Min value of short type {3}\n" +
                 "Max value of int type = {4}\n" +
                 "Min value of int type = {5}\n" +
@@ -40,7 +24,9 @@ namespace homeTask_1
                 "Max value of uint type = {12}\n" +
                 "Min value of uint type = {13}\n" +
                 "Max value of ulong type = {14}\n" +
-                "Min value of ulong type = {15}\n", bMax, bMin, sMax, sMin, iMax, iMin, lMax, lMin, fMax, fMin, dMax, dMin, uiMax, uiMin, ulMax, ulMin);
+                "Min value of ulong type = {15}\n", byte.MaxValue, byte.MinValue, short.MaxValue, short.MinValue,
+                int.MaxValue, int.MinValue, long.MaxValue, long.MinValue, float.MaxValue, float.MinValue, 
+                double.MaxValue, double.MinValue, uint.MaxValue, uint.MinValue, ulong.MaxValue, ulong.MinValue);
         }
     }
 }
