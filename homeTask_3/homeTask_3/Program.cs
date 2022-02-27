a578f512899dfd05dc4c08c5c07f9cc3730c54df
Maksim Kuznets <kashimby@gmail.com>
2022-02-27 19:10:31 +0300

    homeTask3 v 1.3

--- homeTask_3/homeTask_3/Program.cs
+++ homeTask_3/homeTask_3/Program.cs
@@ -4,7 +4,6 @@ namespace homeTask_3
 {
     class Program
     {
-
         //Задание- создать три массива по 10 элементов, первый
         //заполнить значениями индексов элементов, второй - квадратами индексов,
         //третий-кубами, и вывести на экран средние значения элементов для всех массивов
@@ -12,22 +11,17 @@ namespace homeTask_3
         static void InitializeArray(int[] array, int n)
         {
             for (int i = 0; i < array.Length; i++)
-            
                 array[i] = (int)Math.Pow(i+1, n);
-                        
         }
 
         static double AverageSumArray(int[] array)
         {
-            double sum = 0;
+            int sum = 0;
             for (int i = 0; i < array.Length; i++)
-            
                 sum += array[i];
             
-            return sum / array.Length;
+            return 1d * sum / array.Length;
         }
-
-
 
         static void Main(string[] args)
         {
@@ -37,12 +31,9 @@ namespace homeTask_3
             InitializeArray(array1, 1);
             InitializeArray(array2, 2);
             InitializeArray(array3, 3);
-            Console.WriteLine("Average sum for array1 ={0} ", AverageSumArray(array1));
-            Console.WriteLine("Average sum for array2 ={0} ", AverageSumArray(array2));
-            Console.WriteLine("Average sum for array3 ={0} ", AverageSumArray(array3));
-
-
-
+            Console.WriteLine("Average sum for array1 = {0} ", AverageSumArray(array1));
+            Console.WriteLine("Average sum for array2 = {0} ", AverageSumArray(array2));
+            Console.WriteLine("Average sum for array3 = {0} ", AverageSumArray(array3));
         }
     }
 }
