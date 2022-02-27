f480e780ae685daa8bef49177bb35eb667ddbc96
Maksim Kuznets <kashimby@gmail.com>
2022-02-28 00:03:34 +0300

    homeTask5 v 1.0

---
+++ homeTask_5/homeTask_5/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeTask_5
+{
+    class Book
+    {
+        private String name = "";
+        private int pageCount;
+        private String uniqueId = "";
+
+        public Book(String name, int pageCount, String uniqueId)
+        {
+            this.name = name;
+            this.pageCount = pageCount;
+            this.uniqueId = uniqueId;
+        }
+        public void SetName(String name)
+        {
+            this.name = name;
+        }
+        public void SetPage(int page)
+        {
+             this.pageCount = page;
+        }
+        public void SetUniqueId(String id)
+        {
+            this.uniqueId = id;
+        }
+        public String GetName()
+        {
+            return this.name;
+        }
+        public int GetPage()
+        {
+            return this.pageCount;
+        }
+        public String GetUId()
+        {
+            return this.uniqueId;
+        }
+
+        public static void PrintAllBooks(Book[] books)
+        {
+            foreach (Book book in books)
+                Console.WriteLine("Name of Book: {0}, Number of pages: {1}, Unique Id: {2}", book.GetName(), book.GetPage(), book.GetUId());
+        }
+    }
+}
---
+++ homeTask_5/homeTask_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace homeTask_5
+{
+    class Program
+    {
+        //Cоздать класс Книга(в нем название (строка), количество страниц (int), уникальный идентификатор (строка)..
+        //Создайте массив книг (5 штук) проинициализируйте его различными объектами типа "Книга",
+        //и выведите информацию о всех книгах в массиве на экран
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Game of thrones", 877, "GLC001");
+            Book book2 = new Book("Lord of the rings", 1077, "KLL777");
+            Book book3 = new Book("Game of thrones part 2", 773, "GLC002");
+            Book book4 = new Book("Godfather", 877, "GDF455");
+            Book book5 = new Book("Death of the Nile", 342, "DOF000");
+            
+            Book[] books = new []{ book1, book2, book3, book4, book5 };
+
+            Book.PrintAllBooks(books);
+
+        } 
+    }
+}
