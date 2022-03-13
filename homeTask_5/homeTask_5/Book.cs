797e1f916c55bc5d8e292bb1d8938b02f2d93dd0
MaksimKuznets <99362623+MaksimKuznets@users.noreply.github.com>
2022-03-14 00:41:11 +0300

    HomeTask_5 (#6)
    * homeTask4 v 1.0
    
    * homeTask4 v 1.1
    
    * homeTask5 v 1.0
    
    * homeTask5 v1.1
    
    * hometask v1.2
    
    * hometask v1.2.1
    
    * homeTask5 v1.3
    
    * homeTask5 v1.4
    
    * homeTask5 v1.5
    
    * homeTask5 v1.6

---
+++ homeTask_5/homeTask_5/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeTask_5
+{
+    class Book
+    {
+        public String Name { get; set; } = "";
+        public int PageCount { get; set; }
+        public String UniqueId { get; set; } = "";
+        public Book(String name, int pageCount, String uniqueId)
+        {
+            this.Name = name;
+            this.PageCount = pageCount;
+            this.UniqueId = uniqueId;
+        }
+        public void PrintBook()
+        {
+                  Console.WriteLine("Book's name:{0} ,Number of pages:{1}, UniqueId: {2}", this.Name, this.PageCount, this.UniqueId);
+        }
+
+    }
+}
---
+++ homeTask_5/homeTask_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homeTask_5
+{
+    class Program
+    {
+        //Cоздать класс Книга(в нем название (строка), количество страниц (int), уникальный идентификатор (строка)..
+        //Создайте массив книг (5 штук) проинициализируйте его различными объектами типа "Книга",
+        //и выведите информацию о всех книгах в массиве на экран
+        static void Main(string[] args)
+        {
+            
+            Book[] books = BooksInformation.GenerateBooksData();
+            foreach (Book item in books)
+            {
+                item.PrintBook();
+            }
+           
+        } 
+    }
+}
