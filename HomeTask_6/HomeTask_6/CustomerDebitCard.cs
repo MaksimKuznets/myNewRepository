7048a5f72aa7b58e6c4ffc99d8a14a91a4efaf6f
MaksimKuznets <99362623+MaksimKuznets@users.noreply.github.com>
2022-03-15 20:11:08 +0300

    HomeTask6 (#9)
    * homeTask6 v1.0
    
    * homeTask6 v1.1

---
+++ HomeTask_6/HomeTask_6/CustomerDebitCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask_6
+{
+    class CustomerDebitCard : Customer
+    {
+        public CustomerDebitCard(string name, string lastname) : base(name, lastname)
+        {
+
+        }
+        public override void Pay()
+        {
+            Console.WriteLine("DebitCard Payment from {0} {1}", this.Name, this.Lastname);
+        }
+    }
+    
+}
---
+++ HomeTask_6/HomeTask_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeTask_6
+{
+   // 1. Создать абстрактный класс Покупатель, у которого есть абстрактный метод Оплатить.
+   // 2. Создать наследников от этого класса- ПОкупатель за наличные, покупатель с
+   // дебетовой картой, и покупатель с кредитной картой.
+   // 3. Реализовать в них метод Оплатить (пусть просто выводит на экран способ оплаты)
+   // 4. Созадать массив из 5 покупателей разного типа, и в цикде вызвать метод Оплатить.
+   // Проверить что каждый оплатил своим способом (сообщения на экране)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerCash cash1 = new CustomerCash("Maksim","Kuznets");
+            CustomerDebitCard debitcard = new CustomerDebitCard("Egor", "Antonovich");
+            CustomerCreditCard creditcard1 = new CustomerCreditCard("Igor","Kheidorov");
+            CustomerCreditCard creditcard2 = new CustomerCreditCard("Sergey", "Kuzhal");
+            CustomerCash cash2 = new CustomerCash("Eugen", "Panasenkov");
+            Customer[] payArray = { cash1, debitcard, creditcard1, creditcard2, cash2 };
+
+            foreach (Customer item in payArray)
+                item.Pay();
+            //cash.Pay();
+        }
+    }
+}
